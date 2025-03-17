a86e22dc1b3a231f0c885dfafd59005145f1effe
elin6776 <164929726+elin6776@users.noreply.github.com>
2025-03-17 12:29:39 -0400

    fixed sign up

--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -1,105 +1,46 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet,TextInput, TouchableOpacity, Image, Keyboard, TouchableWithoutFeedback, ScrollView, Alert } from "react-native";
+import { StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { useRouter } from 'expo-router';
-import { getAuth, createUserWithEmailAndPassword } from '@react-native-firebase/auth';
-import { getApp } from '@react-native-firebase/app';
+import { signUpUser } from '../../src/firebase/firebaseCrud'; 
 
-export default function signUp() {
+export default function Signup() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState(''); 
   const [username, setUsername] = useState('');
   const [confirm, setConfirm] = useState('');
 
-  const emailSignUp = async () => {
-    if (!email || !password || !username || !confirm) {
-      alert("Please fill out all the information.");
-      return;
-    }
-    if (password !== confirm) {
-      alert("Password and confirm password did not match. Please check again");
-      return;
-    }
-    try {
-      const auth = getAuth(getApp());
-      await createUserWithEmailAndPassword(auth, email, password);
-      Alert.alert("Success", "Registered successfully", [
-        { text: "OK", onPress: () => router.push("/home") }
-      ]);
-    } catch (error) {
-      alert("Unable to sign up: " + error.message);
-    }
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <ScrollView 
-        contentContainerStyle={{ flexGrow: 1, justifyContent: "center", alignItems: "center" }}
-        keyboardShouldPersistTaps="handled"
-      >
-        <View style={registerStyle.container}>
-          <Image source={require('../../assets/images/logo.png')} style={registerStyle.logo} />
+    <View style={loginstyles.container}>
+      <Image source={require('../../assets/images/logo.png')} style={loginstyles.logo} />
 
-          <Text style={registerStyle.header}>Sign up to HabitFlow</Text>
+      <Text style={[loginstyles.header, { marginBottom: 20 }]}>Sign up to HabitFlow</Text>
 
-          <Text style={registerStyle.label}>Username</Text>
-          <TextInput 
-            style={registerStyle.input} 
-            placeholder="Enter a username"
-            onChangeText={setUsername}
-          />
+      <Text style={[loginstyles.label, { marginBottom: 5 }]}>Username</Text>
+      <TextInput style={[loginstyles.input, { marginBottom: 15 }]} placeholder="Enter a username" onChangeText={setUsername} />
 
-          <Text style={registerStyle.label}>Email</Text>
-          <TextInput 
-            style={registerStyle.input} 
-            placeholder="Enter your email" 
-            keyboardType="email-address" 
-            value={email} 
-            onChangeText={setEmail} 
-          />
+      <Text style={[loginstyles.label, { marginBottom: 5 }]}>Email</Text>
+      <TextInput style={[loginstyles.input, { marginBottom: 15 }]} placeholder="Enter your email" keyboardType="email-address" value={email} onChangeText={setEmail} />
 
-          <Text style={registerStyle.label}>Password</Text>
-          <TextInput 
-            style={registerStyle.input} 
-            placeholder="Enter your password" 
-            secureTextEntry={true}  
-            value={password} 
-            onChangeText={setPassword} 
-          />
+      <Text style={[loginstyles.label, { marginBottom: 5 }]}>Password</Text>
+      <TextInput style={[loginstyles.input, { marginBottom: 15 }]} placeholder="Enter your password" secureTextEntry={true} value={password} onChangeText={setPassword} />
 
-          <Text style={registerStyle.label}>Confirm Password</Text>
-          <TextInput 
-            style={registerStyle.input} 
-            placeholder="Enter your password again" 
-            secureTextEntry={true}  
-            value={confirm} 
-            onChangeText={setConfirm} 
-          />
+      <Text style={[loginstyles.label, { marginBottom: 5 }]}>Confirm Password</Text>
+      <TextInput style={[loginstyles.input, { marginBottom: 20 }]} placeholder="Enter your password again" secureTextEntry={true} value={confirm} onChangeText={setConfirm} />
 
-          <TouchableOpacity onPress={emailSignUp} style={registerStyle.signUpButton}>
-            <Text style={registerStyle.signUpText}>Sign up</Text>
-          </TouchableOpacity>
+      <TouchableOpacity onPress={() => signUpUser(email, password, username, confirm, router)} style={[loginstyles.loginButton, { marginBottom: 15 }]}>
+        <Text style={loginstyles.loginText}>Sign up</Text>
+      </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => router.push("/")}>
-            <Text style={registerStyle.loginText}>Already have an account? Log in</Text>
-          </TouchableOpacity>
-     {/* OR Option*/}
-     <View style={registerStyle.orContainer}>
-        <View style={registerStyle.line} />
-        <Text style={registerStyle.orText}>OR</Text>
-        <View style={registerStyle.line} />
-      </View>
-          <TouchableOpacity style={registerStyle.googleButton}>
-            <Image source={require('../../assets/images/google.png')} style={registerStyle.googleIcon} />
-            <Text style={registerStyle.googleText}>Log in with Google</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </TouchableWithoutFeedback>
+      <TouchableOpacity onPress={() => router.push("/")} style={{ marginBottom: 10 }}>
+        <Text style={loginstyles.signupText}>Already have an account? Log in</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
-const registerStyle = StyleSheet.create({
+const loginstyles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
@@ -108,22 +49,22 @@ const registerStyle = StyleSheet.create({
     padding: 20,
   },
   logo: {
-    width: 90,
-    height: 90,
-    marginBottom: 10,
+    width: 80,
+    height: 80,
+    marginBottom: 20,
   },
   header: {
     fontSize: 22,
     fontWeight: "bold",
     color: "#472715",
-    marginBottom: 15,
+    marginBottom: 20,
   },
   label: {
     alignSelf: "flex-start",
     fontSize: 14,
     fontWeight: "bold",
     color: "#4D4D4D",
-    marginBottom: 10,
+    marginBottom: 5,
   },
   input: {
     width: 350,
@@ -133,7 +74,7 @@ const registerStyle = StyleSheet.create({
     paddingHorizontal: 15,
     marginBottom: 15,
   },
-  signUpButton: {
+  loginButton: {
     width: 350,
     height: 50,
     backgroundColor: "#D0E6C1",
@@ -141,33 +82,23 @@ const registerStyle = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 15,
-    marginTop: 5,
   },
-  signUpText: {
+  loginText: {
     fontSize: 16,
     fontWeight: "bold",
     color: "#333",
   },
-  loginText: {
+  signupText: {
     color: "#3468C0",
     textDecorationLine: "underline",
-    marginBottom: 15,
-  },
-  orContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 8,
-  },
-  line: {
-    flex: 1,
-    height: 1,
-    backgroundColor: '#808080',
+    marginBottom: 20,
   },
   orText: {
-    marginHorizontal: 10,
-    color: '#808080',
-    fontWeight: 'bold',
-    marginBottom: 8,
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#7A7A7A",
+    marginVertical: 10,
+    marginBottom: 20,
   },
   googleButton: {
     flexDirection: "row",
