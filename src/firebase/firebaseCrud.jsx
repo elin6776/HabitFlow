f36b23e8f73018ea569dd6dd30f32d681335a03d
elin6776 <164929726+elin6776@users.noreply.github.com>
2025-03-21 21:38:29 -0400

    edited homepage

--- src/firebase/firebaseCrud.jsx
+++ src/firebase/firebaseCrud.jsx
@@ -1,7 +1,7 @@
 import { db, auth } from '../config/firebaseConfig';
 import { getAuth } from '@react-native-firebase/auth';
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { setDoc, doc, collection, addDoc, getDocs, updateDoc } from 'firebase/firestore'; 
+import { setDoc, doc, collection, addDoc, getDocs, getDoc, updateDoc, query, where, deleteDoc } from 'firebase/firestore'; 
 import { useRouter } from 'expo-router';
 
 export const signUpUser = async (email, password, username, confirm, router) => {
@@ -23,8 +23,6 @@ export const signUpUser = async (email, password, username, confirm, router) => 
       username: username,
       email: email,
       createdAt: new Date().toISOString(),
-      accepted_challenges: [],
-      daily_tasks: [],
       points: 0,
     });
 
@@ -35,7 +33,11 @@ export const signUpUser = async (email, password, username, confirm, router) => 
   }
 };
 
-export const fetchTasks = async () => {
+
+//Homepage
+
+//Fetch daily tasks
+export const fetchDailyTasks = async () => {
   try {
     const auth = getAuth();
     const user = auth.currentUser; 
@@ -57,6 +59,71 @@ export const fetchTasks = async () => {
     throw error;
   }
 };
+
+// Create daily task
+export const addDailyTask = async ({ title, time, repeat_days }) => {
+  try {
+    const auth = getAuth();
+
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("No user is signed in");
+      return;
+    }
+    //console.log("Current user UID: ", user.uid);
+
+    const tasksRef = collection(db, "users", user.uid, "daily_tasks");
+
+    await addDoc(tasksRef, {
+      title,
+      time,
+      repeat_days,
+      is_completed: false,
+      createdAt: new Date(),
+    });
+
+    //console.log("Task added successfully");
+  } catch (error) {
+    console.error("Error adding task:", error.message); 
+  }
+};
+
+
+// Update
+// Delete
+export const deleteDailyTask = async (taskUid) => {
+  try {
+    if (!taskUid) {
+      console.log("Task UID is undefined");
+      return;
+    }
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("No user is signed in");
+      return;
+    }
+
+    const taskRef = doc(db, "users", user.uid, "daily_tasks", taskUid);
+    const taskSnap = await getDoc(taskRef);
+
+    if (!taskSnap.exists()) {
+      console.log("Task not found");
+      return;
+    }
+
+    await deleteDoc(taskRef);
+    //console.log("Daily task deleted successfully");
+
+  } catch (error) {
+    console.error("Error deleting daily task:", error.message);
+  }
+};
+
+
 
 export const toggleTaskCompletion = async (taskId, currentStatus, setTasks) => {
   try {
@@ -86,11 +153,114 @@ export const toggleTaskCompletion = async (taskId, currentStatus, setTasks) => {
   }
 };
 
+//Challenges Page
 
-export const addDailyTask = async ({ title, time, repeat_days }) => {
+//Fetch Challenges
+export const fetchChallenges = async () => {
+  try {
+    const challengesCollection = collection(db, "challenges");
+    const challengesSnapshot = await getDocs(challengesCollection);
+
+    return challengesSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error fetching challenges:", error);
+    throw error;
+  }
+};
+
+export const fetchAcceptedChallenges = async () => {
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("No user is signed in");
+      return [];
+    }
+    const userId = user.uid;
+    const acceptedCollection = collection(db, "users", userId, "accepted_challenges");
+    const acceptedSnapshot = await getDocs(acceptedCollection);
+
+    return acceptedSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+  } catch (error) {
+    console.error("Error fetching accepted challenges:", error.message);
+    return [];
+  }
+};
+
+export const filterChallenges = async () => {
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("No user is signed in");
+      return [];
+    }
+
+    const acceptedRef = collection(db, "users", user.uid, "accepted_challenges");
+    const querySnapshot = await getDocs(acceptedRef);
+
+    return querySnapshot.docs.map((doc) => doc.data().challengeId);
+  } catch (error) {
+    console.error("Error fetching accepted challenges:", error.message);
+    return [];
+  }
+};
+
+//Create Challenge
+export const addChallenge = async ({ title, description, duration, task, frequency }) => {
   try {
     const auth = getAuth();
 
+    const user = auth.currentUser;
+    if (!user) {
+      console.log("No user is signed in");
+      return;
+    }
+    const userID = user.uid;
+    //console.log("Current user UID: ", user.uid);
+    
+    let points; 
+    if (duration === 14) {  
+      points = 20;  
+    } else if (duration === 21) {  
+      points = 33;  
+    } else if (duration === 28) {  
+      points = 48;  
+    } else {  
+      points = 9;  
+    }
+    
+    const tasksRef = collection(db, "challenges");
+    await addDoc(tasksRef, {
+      userID,
+      title,
+      description,
+      duration,
+      task,
+      frequency,
+      points,
+      createdAt: new Date(),
+    });
+
+    //console.log("Task added successfully");
+  } catch (error) {
+    console.error("Error adding task:", error.message); 
+  }
+};
+
+
+export const acceptChallenge = async ({ challengeUid }) => {
+  try {
+    const auth = getAuth();
     const user = auth.currentUser;
 
     if (!user) {
@@ -99,18 +269,102 @@ export const addDailyTask = async ({ title, time, repeat_days }) => {
     }
     //console.log("Current user UID: ", user.uid);
 
-    const tasksRef = collection(db, "users", user.uid, "daily_tasks");
+    const duplicateRef = collection(db, "users", user.uid, "accepted_challenges");
+    const q = query(duplicateRef, where("challengeId", "==", challengeUid));
+    const querySnapshot = await getDocs(q);
 
-    await addDoc(tasksRef, {
+    if (!querySnapshot.empty) {
+      console.log("Challenge already accepted");
+      return;
+    }
+
+    const challengeRef = doc(db, "challenges", challengeUid); 
+    const challengeSnap = await getDoc(challengeRef);
+
+    if (!challengeSnap.exists()) {
+      console.log("Challenge not found");
+      return;
+    }
+    
+    const challengeData = challengeSnap.data(); 
+    const { title, description, task, points, duration, frequency } = challengeData;
+
+    const acceptedRef = collection(db, "users", user.uid, "accepted_challenges");
+
+    await addDoc(acceptedRef, {
+      challengeId: challengeUid,  
       title,
-      time,
-      repeat_days,
-      is_completed: false,
-      createdAt: new Date(),
+      description,
+      task,
+      duration,
+      frequency,
+      points: points || 0,         
+      status: "incomplete",       
+      acceptedAt: new Date()       
     });
 
-    //console.log("Task added successfully");
+    //console.log("Challenge added successfully");
   } catch (error) {
-    console.error("Error adding task:", error.message); 
+    console.error("Error adding challenge:", error.message); 
+  }
+};
+
+export const deleteChallenge = async ({ challengeUid }) => {
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("No user is signed in");
+      return;
+    }
+
+    const challengeRef = doc(db, "challenges", challengeUid);
+    const challengeSnap = await getDoc(challengeRef);
+
+    if (!challengeSnap.exists()) {
+      console.log("Challenge not found");
+      return;
+    }
+
+    const challengeData = challengeSnap.data();
+    if (challengeData.userID !== user.uid) {
+      console.log("You are not authorized to delete this challenge");
+      return;
+    }
+
+    await deleteDoc(challengeRef);
+    console.log("Challenge deleted successfully");
+
+  } catch (error) {
+    console.error("Error deleting challenge:", error.message);
+  }
+};
+
+//Deletes accepted challenges
+export const deleteAcceptedChallenge = async ({ challengeUid }) => {
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("No user is signed in");
+      return;
+    }
+
+    const acceptedRef = collection(db, "users", user.uid, "accepted_challenges");
+    const q = query(acceptedRef, where("challengeId", "==", challengeUid));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      console.log("Challenge not found");
+      return;
+    }
+
+    await batch.commit();
+    console.log("Challenge deleted successfully");
+    
+  } catch (error) {
+    console.error("Error deleting challenge:", error.message);
   }
 };
