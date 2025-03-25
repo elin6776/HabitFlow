5dcf131216a95346f84a28d7a2e57af9f9ecc46b
MinimumC <xiaominchen1114@gmail.com>
2025-03-24 20:06:43 -0400

    display color

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Redirect } from "expo-router";
 
 export default function Page() {
-  return <Redirect href={"/(auth)/login"} />;
-  // return <Redirect href={"/(tabs)/challenges"} />;
+  // return <Redirect href={"/(auth)/login"} />;
+  return <Redirect href={"/(tabs)/challenges"} />;
 }
